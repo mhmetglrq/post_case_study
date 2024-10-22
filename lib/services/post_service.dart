4a4a04028824b1d514c0111305512c75373cc373
Mehmet Guler <mhmetglr.q@gmail.com>
2024-10-22 16:12:40 +0300

    Add PostService class for fetching posts and post details

---
+++ lib/services/post_service.dart
@@ -0,0 +1,23 @@
+import 'package:get/get_connect.dart';
+import 'package:post_case_study/config/constants/api_constants.dart';
+import '../models/post_model.dart';
+
+class PostService extends GetConnect {
+  static Future<List<PostModel>?> fetchPosts() async {
+    final response = await GetConnect().get(ApiConstants.BASE_URL+ApiConstants.POSTS);
+    if (response.status.hasError) {
+      return Future.error(response.statusText!);
+    } else {
+      return (response.body as List).map((json) => PostModel.fromJson(json)).toList();
+    }
+  }
+
+  static Future<PostModel?> fetchPostDetail(int id) async {
+    final response = await GetConnect().get('${ApiConstants.BASE_URL}${ApiConstants.POSTS}/$id');
+    if (response.status.hasError) {
+      return Future.error(response.statusText!);
+    } else {
+      return PostModel.fromJson(response.body);
+    }
+  }
+}
